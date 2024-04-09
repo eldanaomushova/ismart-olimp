759f7b31ca7259b1b3455772cb96680675ea080d
eldanaomushova <eldana956@gmail.com>
2024-04-09 23:09:03 +0600

    Header and organization partly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import Navbar from './components/navbar';
 import Organization from './components/organization';
+import Footer from './components/footer';
 
 function App() {
   return (
