9651e5fe1df71bcc2fc841546a8a85f3332698b9
Aibiike <aibiyke.omorova@gmail.com>
2024-04-14 01:22:35 +0600

    drone racing added

--- src/components/organization.js
+++ src/components/organization.js
@@ -16,29 +16,23 @@ const organization = () => {
                     <h2 className='photo-desc'>LEGO EV3</h2>
                     <div className='info'>
                         <a href='https://drive.google.com/file/d/1isLFoq5XWdJ5YhwMGeB8UwEOXW-WzF_f/view'><button className='types' id='competitions'>Sumo</button></a>
-                        <p>Участникам необходимо подготовить автономного робота,
-                            способного наиболее эффективно вытолкнуть робота-противника
-                            за пределы круга, очерчивающего ринг - за черную линию ринга.</p>
+                        <p>Участникам необходимо подготовить автономного робота, способного наиболее эффективно вытолкнуть робота-противника за пределы круга, очерчивающего ринг - за черную линию ринга.</p>
                         <a href='https://drive.google.com/file/d/1sht1u2ARG2bPB4hDJUA3s_zYybE6swzI/view'><button className='types'>Line follower</button></a>
-                        <p>Для соревнований роботов участникам необходимо
-                            подготовить автономного робота, способного проехать от
-                            старта до финиша, следуя черной линии.</p>
+                        <p>Для соревнований роботов участникам необходимо подготовить автономного робота, способного проехать от старта до финиша, следуя черной линии.</p>
                         <a href='https://drive.google.com/file/d/1-2RWrnwc8J4zCHITdGUPk9gH9QCUkcqy/view?usp=drive_link'><button className='types'>Кегель ринг</button></a>
-                        <p>За наиболее короткое время робот, не выходя более чем на 5 секунд за
-                            пределы круга, очерчивающего ринг, должен вытолкнуть
-                            расположенные в нем кегли.</p>
+                        <p>За наиболее короткое время робот, не выходя более чем на 5 секунд за пределы круга, очерчивающего ринг, должен вытолкнуть расположенные в нем кегли.</p>
                         <a href='https://drive.google.com/file/d/1KFknnKECiq-SjbIrDA4h5zLSN710A8TZ/view?usp=sharing'><button className='types'>Кок-бору</button></a>
-                        <p>Кок-бору — это симбиоз скачек и игры. Мы предлагаем перенести эту игру на поле состязания
-                            роботов. Две команды борются за захват и удерживание максимального количества животных.</p>
+                        <p>Кок-бору — это симбиоз скачек и игры. Мы предлагаем перенести эту игру на поле состязания роботов. Две команды борются за захват и удерживание максимального количества животных.</p>
                     </div>
                 </div>
                     <div className='container'>
                         <img src={img2} alt="DRONE" />
                         <h2 className='photo-desc'>DRONE</h2>
                         <div className='info'>
-                            <a href='https://docs.google.com/document/d/1HFoQ5DoowgoUA8nY-cxrhU3geOj2TNRc/edit#heading=h.gjdgxs'><button className='types'>Соревнования дронов</button></a>
-                            <p>Роботу необходимо, перемещаясь по воздуху в ограниченном пространстве, 
-                                выполнить задания облета препятствий, пролета вдоль линии, посадки на поверхность.</p>
+                            <a href='https://docs.google.com/document/d/1HFoQ5DoowgoUA8nY-cxrhU3geOj2TNRc/edit#heading=h.gjdgxs'><button className='types'>Дрон-полоса препятствий</button></a>
+                            <p>Роботу необходимо, перемещаясь по воздуху в ограниченном пространстве, выполнить задания облета препятствий, пролета вдоль линии, посадки на поверхность.</p>
+                            <a href='https://docs.google.com/document/d/1e51Mq1ZV2aiLICJuLX7e-EskqMdnReugwmpDVSTLzdU/edit?usp=sharing'><button className='types'>Дрон-рейсинг</button></a>
+                            <p>Дроны проходят через трассу с препятствиями, стараясь пройти её как можно быстрее.</p>
                         </div>
                     </div>
                 <div className='container'>
@@ -54,15 +48,13 @@ const organization = () => {
                     <h2 className='photo-desc'>LEGO WEDO 2.0</h2>
                     <div className='info'>
                         <a href='https://drive.google.com/file/d/131CVESawqDdYHYwIT3TOI5ZojDbBkEBy/view'><button className='types'>Гонки</button></a>
-                        <p>В этом состязании участникам необходимо подготовить автономного мобильного робота,
-                                способного наиболее быстро проехать от старта до финиша.</p>
+                        <p>В этом состязании участникам необходимо подготовить автономного мобильного робота, способного наиболее быстро проехать от старта до финиша.</p>
                         <a href='https://drive.google.com/file/d/1xFf2M-KPyiPVyxag1sLmhVK6UJ_ojfm1/view'><button className='types'>Перетягивания каната</button></a>
                         <p>Участникам необходимо собрать робота, который сможет перетянуть робота соперника на свою половину используя канат.</p>
                         <a href='https://drive.google.com/file/d/1Z59S8FCgoZIqq8khxzKd1htHrTdKcOm7/view?usp=drive_link'><button className='types'>Сборка "Майло"</button></a>
                         <p>Участникам необходимо успешно собрать робота "Майло" по памяти и на время.</p>
-                        <a href='https://drive.google.com/drive/folders/11R1Ucjqq50cQhWmX_0mWDhHqP8N5LhO0'><button className='types'>Вездеход-полоса препятствий</button></a>
-                        <p>В этом состязании участникам необходимо подготовить автономного мобильного робота,
-                                cпособного наиболее быстро проехать от старта до финиша.</p>
+                        <a href='https://drive.google.com/file/d/1VAfFqzNbloXxPlIesla-Jr043kp2kYow/view'><button className='types'>Вездеход-полоса препятствий</button></a>
+                        <p>В этом состязании участникам необходимо подготовить автономного мобильного робота, cпособного наиболее быстро проехать от старта до финиша.</p>
                     </div>
                     </div>
             </div>
